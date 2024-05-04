967ab909505c070ab88e32dbfc620deac529c45d
Valerie Valentin <valerie.valentine93@gmail.com>
2024-05-04 16:01:28 -0700

    Front end deployment WIP

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <UserProvider>
         <ThemeProvider theme={theme}>
           <App />
